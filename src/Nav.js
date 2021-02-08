c5a9644f37197b290d7024d7945dfd8b81ba3ad5
An Doan <dtan42@gmail.com>
2021-02-08 15:26:20 +1000

    front end and api

---
+++ src/Nav.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+import './Nav.css'
+function Nav() {
+    const [show, setShow] = useState(false)
+
+    const transitionNavBar = () => {
+        if (window.scrollY > 100) {
+            setShow(true)
+        } else {
+            setShow(false)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", transitionNavBar)
+        return () => window.removeEventListener('scroll', transitionNavBar)
+    }, [])
+    return (
+        <div className={`nav ${show && "nav__black"}`}>
+            <div className="nav__contents">
+
+                <img src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt=""
+                    className="nav__logo" />
+                <img src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt=""
+                    className="nav__avatar" />
+            </div>
+        </div>
+    )
+}
+
+export default Nav
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCj5Kihh1TiERRa41gviCXyaoQJDa_AWwA",
+    authDomain: "netflix-build-clone-60c6f.firebaseapp.com",
+    projectId: "netflix-build-clone-60c6f",
+    storageBucket: "netflix-build-clone-60c6f.appspot.com",
+    messagingSenderId: "436090598181",
+    appId: "1:436090598181:web:310eb44f0269eb66baa0cc"
+};
